22703c90eff419cf70ab67f83186f013bd84f396
daheige <zhuwei313@hotmail.com>
2020-11-07 19:25:07 +0800

    gofmt

--- prom/prometheus.go
+++ prom/prometheus.go
@@ -7,37 +7,37 @@ import (
 
 var (
 	// LibClient for mc,redis,db(mysql)
-	LibClient = New().WithTimer("go_lib_client", []string{"method","name"}).
-		WithState("go_lib_client_state", []string{"method", "name"}).
-		WithCounter("go_lib_client_code", []string{"method", "code"})
+	LibClient = New().WithTimer("go_lib_client", []string{"method", "name"}).
+			WithState("go_lib_client_state", []string{"method", "name"}).
+			WithCounter("go_lib_client_code", []string{"method", "code"})
 
 	// RPCClient rpc client
-	RPCClient = New().WithTimer("go_rpc_client", []string{"method","name"}).
-		WithState("go_rpc_client_state", []string{"method", "name"}).
-		WithCounter("go_rpc_client_code", []string{"method", "name","code"})
+	RPCClient = New().WithTimer("go_rpc_client", []string{"method", "name"}).
+			WithState("go_rpc_client_state", []string{"method", "name"}).
+			WithCounter("go_rpc_client_code", []string{"method", "name", "code"})
 
 	// RPCServer for rpc server
 	RPCServer = New().WithTimer("go_rpc_server", []string{"method", "name"}).
-		WithCounter("go_rpc_server_code", []string{"method", "name", "code"})
+			WithCounter("go_rpc_server_code", []string{"method", "name", "code"})
 
 	// HTTPClient http client
-	HTTPClient = New().WithTimer("go_http_client", []string{"method","name"}).
-		WithState("go_http_client_state", []string{"method", "name"}).
-		WithCounter("go_http_client_code", []string{"method", "name","code"})
+	HTTPClient = New().WithTimer("go_http_client", []string{"method", "name"}).
+			WithState("go_http_client_state", []string{"method", "name"}).
+			WithCounter("go_http_client_code", []string{"method", "name", "code"})
 
 	// HTTPServer for http server
 	HTTPServer = New().WithTimer("go_http_server", []string{"method", "name"}).
-		WithCounter("go_http_server_code", []string{"method", "name", "code"})
+			WithCounter("go_http_server_code", []string{"method", "name", "code"})
 
 	// APPErrorCount for business err count
 	// RBI monitoring for business errors
 	APPErrorCount = New().WithCounter("go_app_error_count", []string{"name"}).
-					WithState("go_app_error_state", []string{"name"})
+			WithState("go_app_error_state", []string{"name"})
 
 	// APPInfoCount for business info count
 	// routine monitoring of normal business
 	APPInfoCount = New().WithCounter("go_app_info_count", []string{"name"}).
-		WithState("go_app_info_state", []string{"name"})
+			WithState("go_app_info_state", []string{"name"})
 
 	// CacheHit for cache hit
 	CacheHit = New().WithCounter("go_cache_hit", []string{"name"})
@@ -46,17 +46,17 @@ var (
 	CacheMiss = New().WithCounter("go_cache_miss", []string{"name"})
 
 	// DBQuery db query time and state
-	DBQuery = New().WithTimer("go_db_query", []string{"method","name"}).
-		WithSummary("go_db_query_summary", []string{"method","name"}).
+	DBQuery = New().WithTimer("go_db_query", []string{"method", "name"}).
+		WithSummary("go_db_query_summary", []string{"method", "name"}).
 		WithState("go_db_query_state", []string{"method", "name"})
 )
 
 // Prom struct.
 type Prom struct {
-	timer   *prometheus.HistogramVec
+	timer        *prometheus.HistogramVec
 	timerSummary *prometheus.SummaryVec
-	counter *prometheus.CounterVec
-	state   *prometheus.GaugeVec
+	counter      *prometheus.CounterVec
+	state        *prometheus.GaugeVec
 }
 
 // New creates a Prom instance.
